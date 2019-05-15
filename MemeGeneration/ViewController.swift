50c46f30d83e8f7e756f15d16aca86e43a484992
Michele Galvagno <michicello@gmail.com>
2019-05-15 19:28:13 +0200

    First commit - import picture completed

--- MemeGeneration/ViewController.swift
+++ MemeGeneration/ViewController.swift
@@ -8,13 +8,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    @IBOutlet var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func importPicture(_ sender: Any) {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+    
+    @IBAction func setTopText(_ sender: Any) {
+        
+    }
+    
+    @IBAction func setBottomText(_ sender: Any) {
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.editedImage] as? UIImage else { return }
+        
+        dismiss(animated: true)
+        imageView.image = image
+    }
 }
 
